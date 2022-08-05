d4c65290e225193a92a3619cc1a36c1a9ec06c77
kseniya.bolshakova <ks.bo.vr@gmail.com>
2022-08-05 08:57:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myfirstproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        val info = printAppInfo(Log.d("Report", "App Info: co.android.someappkotlin v1.0(1)"))
+//        println(info)
+        Log.d("Report", printAppInfo())
+    }
+    private fun printAppInfo(): String {
+        var info =""
+        if (BuildConfig.DEBUG){
+             info = "App Info: com.example.myfirstproject v1.0(1)"
+        }
+        return info
+    }
+}
---
+++ app/src/main/java/com/example/myfirstproject/ui/HomePage.kt
@@ -0,0 +1,4 @@
+package com.example.myfirstproject.ui
+
+class HomePage {
+}
